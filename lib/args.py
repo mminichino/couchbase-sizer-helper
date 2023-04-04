dc223f2b69582192e96f47cb618ecc98a90e2550
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2023-04-04 12:29:25 -0500

    first commit

--- lib/args.py
+++ lib/args.py
@@ -4,11 +4,11 @@
 import argparse
 
 
-def name_arg(value):
+def cloud_arg(value):
     if value in ("aws", "gcp", "azure", "vm"):
         return value
     else:
-        raise argparse.ArgumentTypeError("cloud should be aws, gcp, or azure")
+        raise argparse.ArgumentTypeError("cloud should be aws, gcp, azure, or vm")
 
 
 class Parameters(object):
@@ -18,7 +18,7 @@ class Parameters(object):
         parent_parser.add_argument('-i', '--input', action='store', help="Sizing output", required=True)
         parent_parser.add_argument('-o', '--output', action='store', help="Output file", default="cluster_config.json")
         parent_parser.add_argument('-n', '--name', action='store', help="Name", default="Cluster1")
-        parent_parser.add_argument('-c', '--cloud', action='store', help="Cloud", default="aws")
+        parent_parser.add_argument('-c', '--cloud', action='store', help="Cloud", default="aws", type=cloud_arg)
         parent_parser.add_argument('-S', '--self', action='store_true', help="Self Managed")
         parent_parser.add_argument('-s', '--skip', action='store_true', help="Skip unused indexes")
         parent_parser.add_argument('-d', '--debug', action='store_true', help="Debug output")
